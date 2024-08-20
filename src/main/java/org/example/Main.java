1dbf172952a1117341f9dafba441f8e85c4aa890
DawoudTormos <103008170+DawoudTormos@users.noreply.github.com>
2024-08-21 02:45:06 +0300

    Working with JOptionPane.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -39,7 +39,39 @@ public class Main {
         // end
 
 
-        MainWindow main = new MainWindow();
+        //JOptionPane = pop up a standard dialog box that prompts users for a value
+        //				or informs them of something.
+
+        JOptionPane.showMessageDialog(null, "This is a message dialog box", "title", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Here is some useless info", "title", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "really?", "title", JOptionPane.QUESTION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Your computer has a VIRUS!", "title", JOptionPane.WARNING_MESSAGE);
+        JOptionPane.showMessageDialog(null, "CALL TECH SUPPORT OR ELSE!", "title", JOptionPane.ERROR_MESSAGE);
+
+
+        int answer1 = JOptionPane.showConfirmDialog(null, "bro, do you even code?","title",JOptionPane.YES_NO_OPTION);
+        String name = JOptionPane.showInputDialog("What is your name?: ");
+        System.out.println(name);
+
+        String[] responses = {"No, you are!","thank you!","*blush*"};
+        int answer2 = JOptionPane.showOptionDialog(
+                null,
+                "You are the best! :D",
+                "Secret message",
+                JOptionPane.DEFAULT_OPTION,
+                0,
+                icon,
+                responses,
+                responses[0]);
+        System.out.println(answer1);
+        System.out.println(answer2);
+
+
+
+        //commit:Working with going between frames. using frame.dispose(), new frame objects and actionlisteneres.
+        // MainWindow and NewWindow classes are used here.
+        /*
+        MainWindow main = new MainWindow();*/
 
 
 
